4003371a4e7b0b7514e98cda2a2e35a624588c84
Trey_Daviz <treywesleydavis@gmail.com>
2023-01-20 11:34:21 -0600

    Morboord Logging Push

--- working_files/client.py
+++ working_files/client.py
@@ -13,7 +13,7 @@ load_dotenv()
 def call_database(the_name):
     try:
         print("Will try to call ...")
-        with grpc.insecure_channel(os.getenv("TREY_DESKTOP_IP") + ':1') as channel:
+        with grpc.insecure_channel(os.getenv("IP") + ':1') as channel:
             stub = demo_pb2_grpc.DatabaseCallStub(channel)
             response = stub.DBCall(demo_pb2.DatabaseRequest(firstname=the_name))
         return str(response.lastname)
@@ -24,7 +24,7 @@ def call_database(the_name):
 @log(__file__)
 def get_student_info_from_name(the_id):
     print("Will try to call ...")
-    with grpc.insecure_channel(os.getenv("TREY_DESKTOP_IP") + ':1') as channel:
+    with grpc.insecure_channel(os.getenv("IP") + ':1') as channel:
         stub = demo_pb2_grpc.DatabaseCallStub(channel)
         response = stub.InfoFromID(demo_pb2.InfoRequest(id=the_id))
     return response.name
@@ -33,7 +33,7 @@ def get_student_info_from_name(the_id):
 @log(__file__)
 def test_connection(word):
     print("Will try to call ...")
-    with grpc.insecure_channel(os.getenv("TREY_LAPTOP_IP") + ':2') as channel:
+    with grpc.insecure_channel(os.getenv("TREY_TEMP_IP") + ':2') as channel:
         stub = demo_pb2_grpc.TestStub(channel)
         response = stub.Test(demo_pb2.TestRequest(word=word))
     return response.reply
